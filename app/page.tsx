d685acf1f05ed50fa8f9605dcc7ad90c21239aca
fazt <fazt@faztweb.com>
2025-11-14 11:42:26 -0500

    any type fix

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function Page() {
       <h1>Products</h1>
 
       <div className="grid grid-cols-3 gap-4 p-20 container mx-auto">
-        {products.map((product) => (
+        {products.map((product: any) => (
           <div className="bg-white p-5 text-black">
             <h1>{product.name}</h1>
             <img src={product.imageURL} alt="" />
